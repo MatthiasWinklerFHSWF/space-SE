051c4839d8dc866566234ace81288ee86418749a
MatthiasWinklerFHSWF <winkler.matthias@fh-swf.de>
2022-06-08 15:22:03 +0200

    Intial Commit UI

---
+++ src/Domainmodell/Raum.java
@@ -0,0 +1,15 @@
+package Domainmodell;
+
+public class Raum {
+
+    int raumID;
+    int sitzplaetze;
+    boolean  beamer;
+    boolean tafelWhiteBoard;
+    int anzahlComputer;
+    String gebaeude;
+    String etage;
+
+
+
+}
