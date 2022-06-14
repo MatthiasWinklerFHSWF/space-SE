2363e4c4f9978e3293ba637ba736764661f43cdc
leoniexr <leonie.m.rouge@gmail.com>
2022-06-14 12:53:02 +0200

    Erweiterung

--- src/Domainmodell/Buchung.java
+++ src/Domainmodell/Buchung.java
@@ -1,4 +1,19 @@
 package Domainmodell;
 
 public class Buchung {
+    int buchungsID;
+    Benutzer benuter;
+    Raum raum;
+    //Date datum;
+    int zeitraum;
+
+
+    // Konstrukter
+    public Buchung(int buchungsID,Benutzer benuter, Raum raum, int zeitraum){
+        this.buchungsID = buchungsID;
+        this.benuter = benuter;
+        this.raum = raum;
+        this.zeitraum = zeitraum;
+    }
+
 }
