90e3b2f52ac568fcdc5fb032fb9d1d58b1d7417c
MatthiasWinklerFHSWF <winkler.matthias@fh-swf.de>
2022-06-14 12:52:54 +0200

    Initial Commit

--- src/Domainmodell/Raum.java
+++ src/Domainmodell/Raum.java
@@ -2,6 +2,7 @@ package Domainmodell;
 
 public class Raum {
 
+    // Raum Variablen
     int raumID;
     int sitzplaetze;
     boolean  beamer;
@@ -9,5 +10,22 @@ public class Raum {
     int anzahlComputer;
     String gebaeude;
     String etage;
+
+    // Konstruktur > erzeugt einen Raum
+    public Raum(int sitzplaetze, boolean beamer, boolean tafelWhiteBoard, int anzahlComputer, String gebaeude, String etage){
+        this.raumID = fortlaufendeID();
+        this.sitzplaetze = sitzplaetze;
+        this.beamer = beamer;
+        this.tafelWhiteBoard = tafelWhiteBoard;
+        this.anzahlComputer = anzahlComputer;
+        this.gebaeude = gebaeude;
+        this.etage = etage;
+    }
+
+    // fortlaufende ID erhöht bei jedem Aufruf die ID um 1 und gibt diese zurück
+    public int fortlaufendeID(){
+        raumID++;
+        return raumID;
+    }
 
 }
