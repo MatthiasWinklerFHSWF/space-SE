cfedc544c535a8f1e68b652ea6e52c180b0d1e4b
MatthiasWinklerFHSWF <winkler.matthias@fh-swf.de>
2022-06-22 14:52:53 +0200

    Commit google-java-format

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -2,26 +2,21 @@ package UI;
 
 import Domainmodell.Benutzerrolle;
 import Domainmodell.Raum;
-import Logic.IDT;
-import Logic.Raumbuchungssystem;
 
 public class Main {
 
-    public static void main(String [] args){
-        // System.out.println("Hallo");
+  public static void main(String[] args) {
+    // System.out.println("Hallo");
 
+    Raum neuerRaum = new Raum(10, true, false, 10, "A", "3B");
+    System.out.print(neuerRaum.getRaumID());
 
-        Raum neuerRaum = new Raum(10, true, false, 10, "A", "3B");
-        System.out.print(neuerRaum.getRaumID());
+    Raum neuerRaumZwei = new Raum(25, true, true, 20, "ABC", "2a");
+    System.out.print(neuerRaumZwei.getRaumID());
+    System.out.println();
 
-        Raum neuerRaumZwei = new Raum(25, true, true, 20,"ABC", "2a");
-        System.out.print(neuerRaumZwei.getRaumID());
-        System.out.println();
-
-
-        Benutzerrolle student = new Benutzerrolle();
-        student.istStudent();
-        System.out.print(student);
-
-    }
+    Benutzerrolle student = new Benutzerrolle();
+    student.istStudent();
+    System.out.print(student);
+  }
 }
