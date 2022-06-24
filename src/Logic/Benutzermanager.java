fa4a6e5392f39913d1281ce75022a4478e9fd444
mkgundelach <gundelach.mikasamuel@fh-swf.de>
2022-06-24 14:57:20 +0200

    Benutzermanager

--- src/Logic/Benutzermanager.java
+++ src/Logic/Benutzermanager.java
@@ -1,7 +1,21 @@
 package Logic;
 
+import Domainmodell.Benutzer;
+
 public class Benutzermanager implements IBenutzermanager{
 
-    public
+    @Override
+    public void addBenutzer(String name, String passwort) {
 
+    }
+
+    @Override
+    public void removeBenutzer(String name) {
+
+    }
+
+    @Override
+    public Benutzer[] getBenutzer(String name) {
+        return new Benutzer[0];
+    }
 }
