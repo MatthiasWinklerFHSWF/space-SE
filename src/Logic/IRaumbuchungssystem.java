8b380d473c71c730a6662328d1f3a1486b6c9729
leoniexr <leonie.m.rouge@gmail.com>
2022-06-24 15:26:48 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Logic/Raummanager.java

--- src/Logic/IRaumbuchungssystem.java
+++ src/Logic/IRaumbuchungssystem.java
@@ -1,4 +1,15 @@
 package Logic;
 
+import Domainmodell.Benutzer;
+import Domainmodell.Buchung;
+import Domainmodell.Raum;
+
+import java.util.Date;
+
 public interface IRaumbuchungssystem {
+    void addBuchung (Benutzer benutzer, Raum raum, Date datum, int zeitraum);
+
+    void removeBuchung(int buchungsID);
+
+    Buchung[] getBuchung();
 }
--- src/Logic/Raumbuchungssystem.java
+++ src/Logic/Raumbuchungssystem.java
@@ -1,8 +1,28 @@
 package Logic;
 
 
+import Domainmodell.Benutzer;
+import Domainmodell.Buchung;
 import Domainmodell.Raum;
 
+import java.util.Date;
+
 public class Raumbuchungssystem implements IRaumbuchungssystem {
+
+    @Override
+    public Buchung[] getBuchung() {
+        return new Buchung[0];
+    }
+
+    @Override
+    public void removeBuchung(int buchungsID) {
+
+    }
+
+    @Override
+    public void addBuchung(Benutzer benutzer, Raum raum, Date datum, int zeitraum) {
+
+    }
+
 
 }
