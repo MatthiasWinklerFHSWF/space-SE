1063e2a34952c5ad54a909728cbc45af5d911c5f
mkgundelach <gundelach.mikasamuel@fh-swf.de>
2022-07-28 21:26:31 +0200

    Testing Versuch

--- src/test/RaummanagerTest.java
+++ src/test/RaummanagerTest.java
@@ -2,7 +2,9 @@ package test;
 
 import Domainmodell.Raum;
 import Logic.Raummanager;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,15 +17,36 @@ public class RaummanagerTest {
     Raum raum3 = new Raum(25, "104");
 
     // Testen ob beim hinzufügen nachher mehr Räume vorhanden sind
-    @Test
-    public void TestAddRoom(){
-        //assertEquals(rm.addRaum(raum1));
-    }
 
     /*
-    // Keine Duplikate
-    //@Test
+    @Test
     public void TestAddRoom(){
+        //Erstellt Objekt welches ins zum Test in Array eingefügt wird
+        ResizingArray resizingArray = new ResizingArray(2);
+
+        //Fügt das Objekt dem Array hinzu
+        resizingArray.addRaum(raum1);
+        //Überprüft ob das Objekt hinzugefügt wurde
+        Assert.isTrue(resizingArray.length()== 1);
+
+        resizingArray.addRaum(raum2);
+        Assert.isTrue(resizingArray.length()== 2);
+
+        //kann nur 2 hinzufügen, da resizing Objekt nur 2 groß
+        resizingArray.addRaum(raum3);
+        Assert.isTrue(resizingArray.length()== 3);
+    }
+
+    public void TestremoveRaum(){
+        ResizingArray resizingArray = new ResizingArray(1);
+
+        resizingArray.addRaum(raum1);
+        Assert.isTrue(resizingArray.length()== 1);
+
+        //RaumID einfügen um den Raum zu löschen
+        resizingArray.removeRaum();
+        Assert.isTrue(resizingArray.length()== 0);
+
 
     }
 
