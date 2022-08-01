be1eb90fc4f9264f1fc757304830def61db1b22b
leoniexr <leonie.m.rouge@gmail.com>
2022-08-01 17:52:33 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Logic/Raummanager.java

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 
   public static void anzeigenWillkommen(Benutzermanager bm, Raummanager rm, Raumbuchungssystem rbs){
-    //String ausgabe = "Willkommen im Raumbuchungssystem der FH SWF" + "\n";
+    //String ausgabe = "Willkommen im Raumbuchungssystem der FH SWF" + "\n";1
     String ausgabe = "bitte wählen Sie aus:" + "\n";
     ausgabe += "1. Login" + "\n";
     ausgabe += "2. Registrieren";
@@ -229,49 +229,6 @@ public class Main {
     // 1. Auswahlmenü anbieten
     anzeigenAuswahlMenu(bm, rm, rbs);
 
-
-    // ToDo: Methode erstellen für Rückgabe bei falscher Eingabe
-    /*
-
-    if (!input.equals("1") || !input.equals("2") || !input.equals("3")){
-      System.out.println("Keine gültige Eingabe");
-      System.out.print(anzeigenAuswahl());
-      input = eingabeConsole();
-    }
-
-
-
-    // 3. Menü wird angezeigt
-    inputB = eingabeConsole();
-
-    // ToDo: in Methode umwandeln
-    do {
-      eingabeAuswahl(inputB);
-      if (inputB.equals("1")){
-        System.out.println("Bitte geben Sie die Anzahl an Sitzplätzen an:");
-        String inputSitz = eingabeConsole();
-        int inputSitzInt = Integer.parseInt(inputSitz);
-        System.out.println("Bitte geben Sie die Raumnummer an:");
-        String inputRaumnummer = eingabeConsole();
-        rm.addRaum(rm.createRaum(inputSitzInt, inputRaumnummer));
-      }
-
-      if (inputB.equals("3")){
-        System.out.print(rm.showRaumuebersicht());
-      }
-
-      System.out.println(anzeigenAuswahlRaum());
-      inputB = eingabeConsole();
-
-    } while(inputA.equals("3"));
-  */
-    // 3. Menü wird angezeigt
-    // inputB = eingabeConsole();
-    // eingabeAuswahlRaum(inputB);
-
-
-
-
   }
 
 
@@ -283,60 +240,6 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    // System.out.println("Hallo");
-
-    // System.out.print(eingabeConsole());
-
-    /*
-    Raum raum1 = new Raum(10,"10");
-    Raum raum2 = new Raum(20,"20");
-    Raum raum3 = new Raum(30,"30");
-
-    IRaummanager rm = new Raummanager();
-
-    rm.addRaum(raum1);
-    rm.addRaum(raum2);
-    rm.addRaum(raum3);
-
-
-
-    System.out.println(rm.showRaumuebersicht());
-    System.out.println("Hier leer???");
-    rm.getRaum();
-    System.out.println(rm.showRaumuebersicht());
-
-    rm.addRaum(raum1);
-
-    // Raum[] rooms = rm.getRaum();
-
-
-    /*
-    IBenutzermanager bm = new Benutzermanager();
-    Benutzer mw = new Benutzer("Matthias", "123");
-    Benutzer mg = new Benutzer("Mika", "1234");
-    Benutzer lr = new Benutzer("Leonie", "12345");
-
-    bm.addBenutzer(mw);
-    bm.addBenutzer(mg);
-    bm.addBenutzer(lr);
-
-    bm.getBenutzer();
-    System.out.print(bm.showBenutzer());
-
-    bm.logInBenutzer();
-    /*
-
-
-    /*
-    // Test Buchung
-    Benutzer benutzer = new Benutzer("Mika", "1234");
-    Raum raum = new Raum(10,"10");
-    Date datum = Buchung.createDate(2020, 5, 12);
-    Buchung abc = new Buchung(benutzer, raum, 1, datum);
-
-    // System.out.println(datum);
-    System.out.println(abc);
-    */
 
     runRMBS();
 
